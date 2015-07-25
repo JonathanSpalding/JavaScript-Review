a81ba98f7898d7941319bb47c8381c196476e24b
Jon Spalding <jon.spalding@hotmail.com>
2015-07-25 15:36:22 -0600

    First push for Project

--- arrayReview.js
+++ arrayReview.js
@@ -2,33 +2,52 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+//code here
 
+function last() {
+  alert(threeItems[threeItems.length - 1]);
+}
+last();
+
+console.log(threeItems);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
-//Loop through evenArray removing all values that aren't even 
+//Loop through evenArray removing all values that aren't even
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
-
+function evens() {
+  for (i = 0; i < evenArray.length; i++) {
+    if (evenArray[i] % 2 !== 0)
+      evenArray.splice(i, 1);
+  }
+}
+evens();
+console.log(evenArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
 
 //below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
-}
+};
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+//code here
 
-
+function random() {
+  var result = false;
+  var randomNumber = Math.floor(Math.random() * (30 - 0) + 0);
+  for (var i = 0; i < randomArray.length; i++) {
+    if (randomNumber === randomArray[i]){
+      result = true;
+    }
+  }
+  alert(result);
+  console.log(randomNumber);
+}
+random();
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
@@ -37,34 +56,57 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+//code here
 
-alert(first) //[1,2,3,4,5];
-alert(second) //[1,2,3,4,5,6,7];
+var second = first.slice();
+second.push(6,7);
 
 
+
+
+alert(first); //[1,2,3,4,5];
+alert(second); //[1,2,3,4,5,6,7];
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
 
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
-var sentence = "Dev Mountain is the best"
+var sentence = "Dev Mountain is the best";
 
-  //code here
+//code here
 
+var sen = sentence.split(" ");
+
+console.log(sen);
+
+var longest = "";
+
+for (i = 0; i < sen.length; i++) {
+  if (sen[i].length > longest.length)
+    longest = sen[i];
+}
+console.log(longest);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
-var myPoem = 'What is a jQuery but a misunderstood object?'
+//write a function called capitalize that takes in the myPoem variable and capitalizes every word
+var myPoem = 'What is a jQuery but a misunderstood object?';
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+//code here
 
+var sen = myPoem.split(" ");
+var result = [];
+function capitalize() {
+  for (i = 0; i < sen.length; i++) {
+    var cap = sen[i].charAt(0).toUpperCase() + sen.slice(1);
+    result.push(cap);
+  }
+}
+capitalize();
+console.log(sen);
+console.log(result);
 
+//NOT DONE YET!!!
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
--- objectReview.js
+++ objectReview.js
@@ -1,19 +1,35 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+//Your code here
+var favoriteThings = {
+    band: "Weezer",
+    food: "Corndogs",
+    person: "Afton",
+    book: "BofM",
+    movie: "Men In Black",
+    holiday: "Independance Day"
 
+};
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+//Your code here
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+favoriteThings.car = "Subaru BRZ";
+favoriteThings.brand = "Spalding Sporting Goods";
 
-  //Your code here
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
+
+//Your code here
+
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Grey";
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+//Your code here
 
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
